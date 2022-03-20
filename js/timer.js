31b4e094888252bc8c06dabe50d42d16c99ce034
Oleg Markhai <oleg987663@gmail.com>
2022-03-20 23:55:46 +0200

    make timer

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,31 @@
 const timersBlock = document.querySelector('.timer__time')
+const deadline = '31 march 2022'
+
+let interval
 
 const updateBlock = () => {
-    const date = new Date()
+    const date = new Date().getTime()
+    const dateDeadline = new Date(deadline).getTime()
+    const timeRemaining = (dateDeadline - date) / 1000
 
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
+    const hours = Math.floor(timeRemaining / 60 / 60)
+    const minutes = Math.floor((timeRemaining / 60) % 60)
+    const seconds = Math.floor(timeRemaining % 60)
 
     const fHours = hours < 10 ? '0' + hours : hours
     const fMinutes = minutes < 10 ? '0' + minutes : minutes
     const fSeconds = seconds < 10 ? '0' + seconds : seconds
 
     timersBlock.textContent = `${fHours} :${fMinutes} :${fSeconds}`
+
+    if (timeRemaining <= 0) {
+        clearInterval(interval)
+        timersBlock.textContent = '00:00:00'
+
+    }
 }
 
 updateBlock()
 
-setInterval(updateBlock, 500)
+interval = setInterval(updateBlock, 500)
+
